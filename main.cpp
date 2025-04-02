15e5c331c4e006ed4c5163ab4857bd88e62b9853
Hnattt <volodymyr.hnatchuk-ki241@nung.edu.ua>
2025-04-02 23:12:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,45 @@
 
 using namespace std;
 
-double calculateFunction(double x, int n) {
-    return pow(x, n) + sin(x); 
+double y(double x, int n) {
+    if (x < 0) {
+        double result = 1;
+        for (int j = 2; j < n - 1; ++j) {
+            result *= (j + x);
+        }
+        return result;
+    } else {
+        double result = 0;
+        for (int i = 0; i < n; ++i) {
+            double prod = 1;
+            for (int j = 0; j < n; ++j) {
+                prod *= (x + i + pow(j, 2));
+            }
+            result += prod;
+        }
+        return result;
+    }
 }
-void getInput(double &start, double &end, double &step, int &n) {
+
+void read_input(double &start, double &end, double &step, int &n) {
     cout << "Введіть початок інтервалу: ";
     cin >> start;
     cout << "Введіть кінець інтервалу: ";
     cin >> end;
-    cout << "Введіть крок зміни аргументу: ";
+    cout << "Введіть крок: ";
     cin >> step;
-    cout << "Введіть значення n: ";
+    cout << "Введіть n: ";
     cin >> n;
 }
+
 int main() {
     double start, end, step;
     int n;
+    read_input(start, end, step, n);
     
-    getInput(start, end, step, n);
-    
-    cout << "\nРезультати обчислення:\n";
     for (double x = start; x <= end; x += step) {
-        cout << "y(" << x << ", " << n << ") = " << calculateFunction(x, n) << endl;
+        cout << "y(" << x << ", " << n << ") = " << y(x, n) << endl;
     }
+    
     return 0;
 }
